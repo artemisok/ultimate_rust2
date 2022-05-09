c70cff25adc89a3d72339cdf030c4e920c05ac9c
artemisok <97893605+artemisok@users.noreply.github.com>
2022-05-09 18:04:00 +0100

    calling it a day

--- exercise/docs/src/lib.rs
+++ exercise/docs/src/lib.rs
@@ -4,6 +4,19 @@
 //
 // Once you've got the documentation here, run `cargo doc --no-deps --open` and take a look!
 
+//! A pumpkin is a cultivar of winter squash that is round with smooth, slightly ribbed skin,
+//!  and is most often deep yellow to orange in coloration.[1] The thick shell contains the seeds and pulp.
+//! The name is most commonly used for cultivars of Cucurbita pepo, but some cultivars of Cucurbita maxima, C. argyrosperma, and C. moschata
+//! with similar appearance are also sometimes called "pumpkins".
+//!
+//! ![Pumpkinella](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg)
+///
+///
+
+///
+///
+///
+
 // 2. What about an image!? Add an image of a pumpkin to the end of the module-level documentation.
 // The markdown format is ![some alt text](https://url-to-the-image.png)
 // Here's the image to link to: https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg
@@ -15,8 +28,15 @@
 // - Document the "roundness" field, explaining that it is a percentage
 // - Document the "orangeness" field, explaining that it is a number from 8 to 27
 
+/// Big Orange Thing
+
+/// # Recipes
+///
+/// Recipes will be updated soon
 pub struct Pumpkin {
+    /// Roundness is the percentage
     pub roundness: f32,
+    /// Orangeness is a number representing the color pattern between 8 to 27
     pub orangeness: i32,
 }
 
@@ -24,12 +44,14 @@ pub struct Pumpkin {
 // can't be used for pie. :'-(
 
 impl Pumpkin {
+    /// If you smash a pumpkin, it will be maybe fun for short term but then you would not be able to make a pie in long term.😭
     pub fn smash(self) {}
 }
 
 // 5. Document that BURNT_ORANGE is for the "orangeness" field in the Pumpkin struct.
 // - Link to the Pumpkin struct in your description
 
+/// Burnt Orange is the color demonstrated with number 13, Please see: [`Pumpkin`]
 pub const BURNT_ORANGE: i32 = 13;
 
 // Challenge: Find the option to pass to `cargo doc` so that documentation for this private item
